11d776d9ab99d3e260beab6af1e327dd082c413b
sdfgx123 <sdfgx123@naver.com>
2023-06-25 17:14:35 +0900

    feat: 야구장, 팀 DAO 초기 세팅

---
+++ src/main/java/dao/StadiumDAO.java
@@ -0,0 +1,22 @@
+package dao;
+
+import java.sql.Connection;
+
+public class StadiumDAO {
+
+    private Connection connection;
+
+    public StadiumDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    // 경기장 등록
+    public void registerStadium() {
+
+    }
+
+    // 경기장 목록
+    public void getStadiumList() {
+
+    }
+}
---
+++ src/main/java/dao/TeamDAO.java
@@ -0,0 +1,22 @@
+package dao;
+
+import java.sql.Connection;
+
+public class TeamDAO {
+
+    private Connection connection;
+
+    public TeamDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    // 팀 등록
+    public void registerTeam() {
+
+    }
+
+    // 팀 목록
+    public void getTeamList() {
+
+    }
+}
