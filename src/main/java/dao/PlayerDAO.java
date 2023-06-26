a40b02f30889ef7496f4e1b10e50101ed995c437
brotherstone97 <brotherstone97@gmail.com>
2023-06-26 16:07:19 +0900

    feat(PlayerDAO): 선수 dao 생성

---
+++ src/main/java/dao/PlayerDAO.java
@@ -0,0 +1,55 @@
+package dao;
+
+import model.Player;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerDAO {
+    private Connection connection;
+
+    public PlayerDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void registerPlayer(int teamId, String name, String position) {
+        String query = "insert into player_tb (team_id, name, position) values (?,?,?)";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, teamId);
+            statement.setString(2, name);
+            statement.setString(3, position);
+
+            int result = statement.executeUpdate();
+            System.out.println("register Player result= " + result);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Player> getPlayers(int teamId) {
+        List<Player> players = new ArrayList<>();
+        String query = "select * from player_tb where team_id=?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, teamId);
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                Player player = new Player(
+                        rs.getInt("id"),
+                        rs.getInt("team_id"),
+                        rs.getString("name"),
+                        rs.getString("position"),
+                        rs.getTimestamp("created_at")
+                );
+                players.add(player);
+            }
+            return players;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
