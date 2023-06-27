fc4cc2aa206adccb1a0ca1393dd9eaf7e319acb2
brotherstone97 <brotherstone97@gmail.com>
2023-06-27 15:28:30 +0900

    fix(PlayerDAO): create_at생성되지 않던 오류 해결

--- src/main/java/dao/PlayerDAO.java
+++ src/main/java/dao/PlayerDAO.java
@@ -5,6 +5,7 @@ import model.Player;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 public class PlayerDAO {
     private Connection connection;
@@ -14,12 +15,13 @@ public class PlayerDAO {
     }
 
     public void registerPlayer(int teamId, String name, String position) {
-        String query = "insert into player_tb (team_id, name, position) values (?,?,?)";
+        String query = "insert into player_tb (team_id, name, position, created_at) values (?,?,?,?)";
         try {
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setInt(1, teamId);
             statement.setString(2, name);
             statement.setString(3, position);
+            statement.setTimestamp(4, new Timestamp(new Date().getTime()));
 
             int result = statement.executeUpdate();
             System.out.println("register Player result= " + result);
