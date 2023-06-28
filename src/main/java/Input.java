3626ebfa08795eee7d30a6a42d555786a0357114
sdfgx123 <sdfgx123@naver.com>
2023-06-28 20:16:34 +0900

    feat: IllegalArgumentException 예외처리 추가

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -5,24 +5,36 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Input {
 
     static Scanner sc = new Scanner(System.in);
 
     public static InputDTO makeRequest() {
+        Pattern pattern = Pattern.compile("^\\w+\\?\\w+=.+(&\\w+=.+)*$");
+
         while (true) {
             System.out.printf("어떤 기능을 요청하시겠습니까? 종료를 원하시면 end를 입력하십시오 : ");
             try {
                 String str = sc.next();
+
                 if (str.equals("end")) {
                     throw new InputEndException("end 입력 : 프로그램을 종료합니다.");
+                }
+
+                Matcher matcher = pattern.matcher(str);
+                if (!matcher.matches()) {
+                    throw new IllegalArgumentException("잘못된 형식입니다. 다시 입력하십시오.");
                 }
                 InputDTO pDTO = parseString(str);
                 return pDTO;
             } catch (InputEndException e) {
                 System.out.println(e.getMessage());
                 System.exit(0);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
