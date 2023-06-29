165493555444078408a0bbc25adfed417c19dfdc
sdfgx123 <sdfgx123@naver.com>
2023-06-29 16:01:06 +0900

    style: 야구장목록 출력 포맷 변경

--- src/main/java/service/StadiumService.java
+++ src/main/java/service/StadiumService.java
@@ -31,8 +31,19 @@ public class StadiumService {
 
     public void getStadiumList() {
         List<Stadium> stadiumList = stadiumDAO.getStadiumList();
-        System.out.println(stadiumList);
-        System.out.println("성공");
+
+        System.out.printf("%-5s | %-20s | %-25s%n", "ID", "이름", "생성 날짜");
+        System.out.println("---------------------------------------------------");
+
+        for (Stadium stadium : stadiumList) {
+            System.out.printf("%-5d | %-20s | %-25s%n",
+                    stadium.getId(),
+                    stadium.getName(),
+                    stadium.getCreatedAt());
+        }
+
+//        System.out.println(stadiumList);
+//        System.out.println("성공");
     }
 
 }
