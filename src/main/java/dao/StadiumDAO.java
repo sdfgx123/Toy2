21a4e97e713c82618b47e63b0ca71e7bee9b7b9f
sdfgx123 <sdfgx123@naver.com>
2023-06-29 19:57:51 +0900

    refactor: registerStadium 함수 예외처리 추가

--- src/main/java/dao/StadiumDAO.java
+++ src/main/java/dao/StadiumDAO.java
@@ -31,14 +31,30 @@ public class StadiumDAO {
         String query = "insert into stadium_tb (name) values (?)";
 
         try {
+            if (name == null) {
+                throw new IllegalArgumentException();
+            }
+
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, name);
 
             int result = statement.executeUpdate();
-            System.out.println("registerStadium res : " + result);
+            return result > 0;
 
+        } catch (IllegalArgumentException e) {
+            System.out.println("유효하지 않은 파라미터 입니다. 요청 파라미터를 확인하십시오.");
+            return false;
+        } catch (SQLIntegrityConstraintViolationException e) {
+            System.out.println("데이터베이스 무결성 제약 조건에 위배됩니다. 요청 파라미터를 확인하십시오.");
+            return false;
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("요청에 문제가 있습니다. 요청 내용을 확인하십시오.");
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("예기치 않은 오류가 발생했습니다. 요청을 다시 확인 후 재시도 하십시오.");
+            return false;
         }
     }
 
