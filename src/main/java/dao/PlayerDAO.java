e5e55d6795b81c0d0d3e9dd23069ca1c8e7d9f7d
brotherstone97 <brotherstone97@gmail.com>
2023-06-29 16:24:42 +0900

    feat(PlayerDAO): 포지션별 팀 야구선수 목록 반환

--- src/main/java/dao/PlayerDAO.java
+++ src/main/java/dao/PlayerDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import dto.PositionRespDTO;
 import model.Player;
 
 import java.sql.*;
@@ -12,7 +13,8 @@ public class PlayerDAO {
     private static PlayerDAO playerDAO;
     private static Connection connection;
 
-    private PlayerDAO() {}
+    private PlayerDAO() {
+    }
 
     public static PlayerDAO getInstance(Connection connection) {
         if (playerDAO == null) {
@@ -62,4 +64,37 @@ public class PlayerDAO {
         }
         return null;
     }
+
+    public List<PositionRespDTO> getPlayersOfPosition() {
+        List<PositionRespDTO> players = new ArrayList<>();
+        String query = "select \n" +
+                "p.position as '포지션'," +
+                "max(case when t.name='두산' then p.name end) as '두산'," +
+                "max(case when t.name='넥센' then p.name end) as '넥센'," +
+                "max(case when t.name='NC' then p.name end) as 'NC'," +
+                "max(case when t.name='기아' then p.name end) as '기아'," +
+                "max(case when t.name='SK' then p.name end) as 'SK'" +
+                "from team_tb t inner join player_tb p on t.id = p.team_id where p.position='1루수' or p.position='2루수'" +
+                "group by p.position;";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                PositionRespDTO positionRespDTO = new PositionRespDTO(
+                        rs.getString("포지션"),
+                        rs.getString("두산"),
+                        rs.getString("넥센"),
+                        rs.getString("NC"),
+                        rs.getString("기아"),
+                        rs.getString("SK")
+                );
+                players.add(positionRespDTO);
+            }
+            return players;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
