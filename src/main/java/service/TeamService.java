4dfa9560a3faa9bd7d0a017c174b4c46b5900581
sdfgx123 <sdfgx123@naver.com>
2023-06-29 16:17:29 +0900

    style: TeamService 결과 출력 포맷 변경

--- src/main/java/service/TeamService.java
+++ src/main/java/service/TeamService.java
@@ -27,13 +27,28 @@ public class TeamService {
         String stadiumId = pDTO.getParameters().get("stadiumId");
         String name = pDTO.getParameters().get("name");
         teamDAO.registerTeam(stadiumId, name);
-        System.out.println("성공");
+        System.out.println("\n--------");
+        System.out.println("팀 등록 성공");
+        System.out.println("--------\n");
+
     }
 
     public void getTeamList() {
         List<TeamRespDTO> teamList = teamDAO.getTeamList();
-        System.out.println(teamList.toString());
-        System.out.println("성공");
+
+        System.out.printf("%-6s | %-12s | %-25s | %-35s | %-25s | %-35s%n",
+                "ID", "Stadium ID", "팀 이름", "생성 날짜", "Stadium 이름", "Stadium 생성 날짜");
+        System.out.println("----------------------------------------------------------------------------------------------------");
+
+        for (TeamRespDTO team : teamList) {
+            System.out.printf("%-6d | %-12d | %-25s | %-35s | %-25s | %-35s%n",
+                    team.getId(),
+                    team.getStadiumId(),
+                    team.getName(),
+                    team.getCreatedAt(),
+                    team.getStadiumName(),
+                    team.getStadiumCreatedAt());
+        }
     }
 
 
